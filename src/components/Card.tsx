d1b58b82715f7f026f2d620b37affdcbe8f72831
James Stomberg <james@vistrom.se>
2025-01-20 23:10:21 +0100

    Add button styles and adjust card styles.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,38 @@
-export default function Card({ title, description, status } : {title: string, description: string, status: object}) {
+export default function Card({
+    title,
+    description,
+    status,
+}: {
+    title: string;
+    description: string;
+    status: object;
+}) {
     return (
-        <div
-            className="site-content__card"
-        >
-            <div className="site-content__card-inner-container">
-                <p>
-                    {description}
-                </p>
+        <div className="site-content__card">
+            <div className="site-content__card-title-container">
+                <div className="site-content__card-delete-button-container">
+                    <button className="site-content__card-button">X</button>
+                </div>
+                <h2>{title}</h2>
             </div>
-            <p>{title}</p>
+            <div className="site-content__card-inner-container">
+                <p>{description}</p>
+            </div>
+            <div>
+                <h3>Status</h3>
+
+                <div className="site-content__card-buttons-container">
+                    <button className="site-content__card-button site-content__card-button--onhold site-content__card-button--inactive">
+                        P
+                    </button>
+                    <button className="site-content__card-button site-content__card-button--inprogress site-content__card-button--inactive">
+                        S
+                    </button>
+                    <button className="site-content__card-button site-content__card-button--completed site-content__card-button--inactive">
+                        C
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,10 @@
 import Card from './Card';
 import '../assets/Cards.scss';
+import { useState } from 'react';
 
 export default function Cards() {
+    const [statuses, setStatuses] = useState([]);
+
     return (
         <div className="site-content__cards">
             {/*<Card key={index} src={src} alt={alt} title={title} />;*/}
