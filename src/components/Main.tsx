2b452c1f544e6a0ca0b904aad2b6312402e20ec0
James Stomberg <james@vistrom.se>
2025-01-20 23:24:40 +0100

    Edit Main component and adjust styles for Footer component.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,16 +3,16 @@ import Cards from './Cards';
 import { useState } from 'react';
 
 export default function Main({ title }: { title: string }) {
-    const [clickedDog, setClickedDog] = useState('');
-
     return (
         <main className="site-content">
             <section>
                 <h1>{title}</h1>
+                <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '0.5rem', flexDirection: 'row'}}>
+                    <p>Lägg till ny ToDo eller hantera befintliga.</p>
+                    <button>+</button>
+                </div>
 
-                {clickedDog && <p>Du har klickat på: {clickedDog}</p>}
-
-                <Cards setClickedDog={setClickedDog} />
+                <Cards />
             </section>
         </main>
     );
